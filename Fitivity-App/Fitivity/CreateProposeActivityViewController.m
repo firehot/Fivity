49d5b73bd6a88ac3250143452c68e1fb519bd462
James Forkey <jmforkey@wpi.edu>
2012-08-27 11:10:00 -0400

    Create activity screen changed

--- Fitivity-App/Fitivity/CreateProposeActivityViewController.m
+++ Fitivity-App/Fitivity/CreateProposeActivityViewController.m
@@ -205,7 +205,7 @@
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
-		[self.navigationItem setTitle:@"New Activity"];
+		self.navigationItem.title = [group objectForKey:@"place"];
         UIBarButtonItem *button = [[UIBarButtonItem alloc] initWithTitle:[NSString stringWithFormat:@"%d", kMaxCharCount] style:UIBarButtonItemStyleBordered target:self action:@selector(post)];
 		[self.navigationItem setRightBarButtonItem:button];
     }
